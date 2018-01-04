d039e250dee40b7068819a7b9856506aa8ade353
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-04 07:21:26 +0700

    Perulangan

---
+++ Persegi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ class Persegi{
+ 	public static void main(String[] args){
+ 		Scanner sc = new Scanner(System.in);
+ 	System.out.print("Nilai : ");
+ 	Integer bo=sc.nextInt();
+
+ 	System.out.println();
+
+     for (int a=1; a<=bo; a++){
+     	for(int j=1; j<=a; j++){
+     	// System.out.println(a);
+
+System.out.print(j);
+     }
+    System.out.println();
+ 	}
+ }
+}
